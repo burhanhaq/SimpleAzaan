bccbeb79871ed6efad5a852c137827cdd8a850e1
Burhan Ul Haq <burhanhaq3@gmail.com>
2023-03-07 19:15:31 -0800

    Update to automatically fetch data in ios timeline provider

--- ios/LockScreenWidget/LockScreenWidget.swift
+++ ios/LockScreenWidget/LockScreenWidget.swift
@@ -23,21 +23,6 @@ struct Provider: TimelineProvider {
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [PrayerEntry] = []
-
-        let sharedDefaults = UserDefaults.init(suiteName: "group.com.simpleAzaan")
-        var prayerData: PrayerData? = nil
-
-        if(sharedDefaults != nil) {
-            do {
-              let shared = sharedDefaults?.string(forKey: "prayerData")
-              if(shared != nil){
-                let decoder = JSONDecoder()
-                prayerData = try decoder.decode(PrayerData.self, from: shared!.data(using: .utf8)!)
-              }
-            } catch {
-              print(error)
-            }
-        }
         
         let isoDateFormatter = ISO8601DateFormatter()
         isoDateFormatter.formatOptions = [
@@ -45,53 +30,84 @@ struct Provider: TimelineProvider {
             .withTime,
             .withColonSeparatorInTime]
         isoDateFormatter.timeZone = TimeZone.current
-        
-        let prayers = [
-            PrayerConfig(
-                prayerType: Prayer.Fajr,
-                timePrayerStarts: isoDateFormatter.date(from: prayerData!.time1)!,
-                timeToShowPrayerIcon: isoDateFormatter.date(from: prayerData!.time6)!
-            ),
-            PrayerConfig(
-                prayerType: Prayer.Sunrise,
-                timePrayerStarts: isoDateFormatter.date(from: prayerData!.time2)!,
-                timeToShowPrayerIcon: isoDateFormatter.date(from: prayerData!.time1)!
-            ),
-            PrayerConfig(
-                prayerType: Prayer.Zuhr,
-                timePrayerStarts: isoDateFormatter.date(from: prayerData!.time3)!,
-                timeToShowPrayerIcon: isoDateFormatter.date(from: prayerData!.time2)!
-            ),
-            PrayerConfig(
-                prayerType: Prayer.Asr,
-                timePrayerStarts: isoDateFormatter.date(from: prayerData!.time4)!,
-                timeToShowPrayerIcon: isoDateFormatter.date(from: prayerData!.time3)!
-            ),
-            PrayerConfig(
-                prayerType: Prayer.Maghrib,
-                timePrayerStarts: isoDateFormatter.date(from: prayerData!.time5)!,
-                timeToShowPrayerIcon: isoDateFormatter.date(from: prayerData!.time4)!
-            ),
-            PrayerConfig(
-                prayerType: Prayer.Isha,
-                timePrayerStarts: isoDateFormatter.date(from: prayerData!.time6)!,
-                timeToShowPrayerIcon: isoDateFormatter.date(from: prayerData!.time5)!
-            )
-        ]
-        
-//        let data = UserDefaults.init(suiteName:"group.com.simpleAzaan")
-//        let m: Int = Int((data?.string(forKey: "id"))!)!
 
-        for pc in prayers {
-            let entry = PrayerEntry(
-                date: pc.timeToShowPrayerIcon,
-                prayerConfig: pc
-            )
-            entries.append(entry)
+        let url = "https://api.aladhan.com/v1/"
+        let timingsByCity = "timingsByCity?"
+        let params = "city=Bellevue&country=US&method=2&iso8601=true"
+        let apiUrl = url+timingsByCity+params
+        var request = URLRequest(url: URL(string: apiUrl)!)
+        request.allHTTPHeaderFields = [
+            "content-Type": "application/x-www-form-urlencoded",
+            "accept": "application/json"]
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if error == nil && data != nil {
+                do {
+                    var newJsonData = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? [String:Any]
+                    print(newJsonData!)
+                    let newData = newJsonData!["data"] as? [String: Any]
+                    let timings = newData!["timings"] as? [String: Any]
+                    let fajr = timings?["Fajr"] as! String
+                    let sunrise = timings?["Sunrise"] as! String
+                    let zuhr = timings!["Dhuhr"] as! String
+                    let asr = timings!["Asr"] as! String
+                    let maghrib = timings!["Maghrib"] as! String
+                    let isha = timings!["Isha"] as! String
+
+
+                    let prayers = [
+                        PrayerConfig(
+                            prayerType: Prayer.Sunrise,
+                            timePrayerStarts: isoDateFormatter.date(from: sunrise)!,
+                            timeToShowPrayerIcon: isoDateFormatter.date(from: fajr)!
+                        ),
+                        PrayerConfig(
+                            prayerType: Prayer.Zuhr,
+                            timePrayerStarts: isoDateFormatter.date(from: zuhr)!,
+                            timeToShowPrayerIcon: isoDateFormatter.date(from: sunrise)!
+                        ),
+                        PrayerConfig(
+                            prayerType: Prayer.Asr,
+                            timePrayerStarts: isoDateFormatter.date(from: asr)!,
+                            timeToShowPrayerIcon: isoDateFormatter.date(from: zuhr)!
+                        ),
+                        PrayerConfig(
+                            prayerType: Prayer.Maghrib,
+                            timePrayerStarts: isoDateFormatter.date(from: maghrib)!,
+                            timeToShowPrayerIcon: isoDateFormatter.date(from: asr)!
+                        ),
+                        PrayerConfig(
+                            prayerType: Prayer.Isha,
+                            timePrayerStarts: isoDateFormatter.date(from: isha)!,
+                            timeToShowPrayerIcon: isoDateFormatter.date(from: maghrib)!
+                        ),
+                        PrayerConfig(
+                            prayerType: Prayer.Fajr,
+                            timePrayerStarts: isoDateFormatter.date(from: fajr)!,
+                            timeToShowPrayerIcon: isoDateFormatter.date(from: isha)!
+                        ),
+                        PrayerConfig(
+                            prayerType: Prayer.Fajr,
+                            timePrayerStarts: isoDateFormatter.date(from: fajr)!,
+                            timeToShowPrayerIcon: isoDateFormatter.date(from: fajr)!
+                        )
+                    ]
+                    for pc in prayers {
+                        let entry = PrayerEntry(
+                            date: pc.timeToShowPrayerIcon,
+                            prayerConfig: pc
+                        )
+                        entries.append(entry)
+                    }
+                    let timeline = Timeline(entries: entries, policy: .atEnd)
+                    completion(timeline)
+                } catch {
+                    print("Error parsing response data")
+                }
+            }
         }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+        task.resume()
     }
 }
 
@@ -114,11 +130,6 @@ struct LockScreenWidgetEntryView : View {
     @Environment(\.widgetFamily) var widgetFamily
 
     var body: some View {
-//        let dateToShow = entry.prayerConfig.timePrayerStarts
-//        let hour = Calendar.current.component(.hour, from: dateToShow)
-//        let minute = Calendar.current.component(.minute, from: dateToShow)
-//        let timeString: String = String(format: "%02d:%02d", hour, minute)
-        
         switch (widgetFamily) {
         case .accessoryCircular:
             CircularWidget(entry: entry)
@@ -188,8 +199,8 @@ struct PrayerView: View {
             MaghribView()
         case Prayer.Isha:
             IshaView()
-        //default:
-        //    SunShape(filled: false)
+//        default:
+//            MosqueShape()
         }
     }
 }
@@ -204,7 +215,7 @@ struct LockScreenWidget: Widget {
         }
         .configurationDisplayName("Small Widget")
         .description("Prayer Time Widget.")
-        .supportedFamilies([.accessoryCircular, .accessoryInline])
+        .supportedFamilies([.accessoryInline, .accessoryCircular])
     }
 }
 
@@ -213,11 +224,11 @@ struct LockScreenWidget_Previews: PreviewProvider {
         let pc = PrayerConfig(prayerType: Prayer.Zuhr, timePrayerStarts: Date(), timeToShowPrayerIcon: Date())
         
         LockScreenWidgetEntryView(entry: PrayerEntry(date: pc.timePrayerStarts, prayerConfig: pc))
-            .previewContext(WidgetPreviewContext(family: .accessoryCircular))
-            .previewDisplayName("Circular")
-        LockScreenWidgetEntryView(entry: PrayerEntry(date: pc.timePrayerStarts, prayerConfig: pc))
             .previewContext(WidgetPreviewContext(family: .accessoryInline))
             .previewDisplayName("Inline")
+        LockScreenWidgetEntryView(entry: PrayerEntry(date: pc.timePrayerStarts, prayerConfig: pc))
+            .previewContext(WidgetPreviewContext(family: .accessoryCircular))
+            .previewDisplayName("Circular")
         
     }
 }
