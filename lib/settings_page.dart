25c5f994e6488252b0ab777e9938f97561069f6c
Burhan Ul Haq <burhanhaq3@gmail.com>
2023-03-28 13:45:15 -0400

    Add placeholder settings page

---
+++ lib/settings_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final screenHeight = size.height;
+    return Container(
+      color: Colors.green,
+      height: screenHeight,
+      width: 6,
+    );
+  }
+}
