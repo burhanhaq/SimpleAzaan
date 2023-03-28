297b4639cd378f6658ee208055f95fc82fcaba9e
Burhan Ul Haq <burhanhaq3@gmail.com>
2023-03-28 13:46:09 -0400

    Update colors to white/green. Use stack on home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,7 @@ import 'package:simple_azaan/aladhan_api.dart';
 import 'package:simple_azaan/prayer.dart';
 import 'package:simple_azaan/prayer_card.dart';
 import 'package:simple_azaan/prayer_data.dart';
+import 'package:simple_azaan/settings_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -46,77 +47,49 @@ class _HomePageState extends State<HomePage> {
     _updatePrayerTime();
 
     var screenSize = MediaQuery.of(context).size;
-    var clockDiameter = screenSize.width / 1.5;
+    var clockDiameter = screenSize.width / 1.3;
 
-    return Row(
-      mainAxisSize: MainAxisSize.max,
+    return Stack(
+      fit: StackFit.expand,
       children: [
-        Expanded(
-          flex: 2,
-          child: Container(
-            color: Colors.red,
-          ),
-        ),
-        Expanded(
-          flex: 100,
-          child: Container(
-            color: Colors.yellow,
-            child: SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    height: clockDiameter,
-                    width: clockDiameter,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.blue,
-                    ),
+        Container(
+          color: const Color(0xfff6f7f9),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  height: clockDiameter,
+                  width: clockDiameter,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xffffffff),
                   ),
-                  Container(
-                    child: Column(children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          PrayerCard(
-                            prayer: fajr ??
-                                Prayer('Fajr',
-                                    DateTime.parse('2020-03-25 00:00:00')),
-                          ),
-                          PrayerCard(
-                            prayer: sunrise ??
-                                Prayer('Sunrise',
-                                    DateTime.parse('2020-03-25 00:00:00')),
-                          ),
-                          PrayerCard(
-                            prayer: zuhr ??
-                                Prayer('Zuhr',
-                                    DateTime.parse('2020-03-25 00:00:00')),
-                          ),
-                          PrayerCard(
-                            prayer: asr ??
-                                Prayer('Asr',
-                                    DateTime.parse('2020-03-25 00:00:00')),
-                          ),
-                          PrayerCard(
-                            prayer: maghrib ??
-                                Prayer('Maghrib',
-                                    DateTime.parse('2020-03-25 00:00:00')),
-                          ),
-                          PrayerCard(
-                            prayer: isha ??
-                                Prayer('Isha',
-                                    DateTime.parse('2020-03-25 00:00:00')),
-                          ),
-                        ],
-                      ),
-                    ]),
+                ),
+                const Spacer(),
+                Column(children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      PrayerCard(prayer: fajr),
+                      PrayerCard(prayer: sunrise),
+                      PrayerCard(prayer: zuhr),
+                      PrayerCard(prayer: asr),
+                      PrayerCard(prayer: maghrib),
+                      PrayerCard(prayer: isha),
+                    ],
                   ),
-                ],
-              ),
+                ]),
+              ],
             ),
           ),
+        ),
+        const Positioned(
+          left: 0,
+          child: SettingsPage(),
         ),
       ],
     );
--- lib/prayer_card.dart
+++ lib/prayer_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:simple_azaan/prayer.dart';
 
 class PrayerCard extends StatefulWidget {
-  final Prayer prayer;
+  final Prayer? prayer;
   const PrayerCard({super.key, required this.prayer});
 
   @override
@@ -16,32 +16,39 @@ class _PrayerCardState extends State<PrayerCard> {
     var cardWidth = screenSize.width * 0.8;
     var cardHeight = cardWidth / 7;
 
-    var prayerPassed = widget.prayer.prayerTime.isBefore(DateTime.now());
+    var prayerPassed =
+        widget.prayer?.prayerTime.isBefore(DateTime.now()) ?? false;
+    var prayerName = widget.prayer?.name ?? 'Prayer';
+    var prayerTime = widget.prayer?.getTimeString() ?? '12:00';
 
     return Container(
       width: cardWidth,
       height: cardHeight,
       padding: const EdgeInsets.all(4),
       margin: const EdgeInsets.symmetric(vertical: 3),
-      color: Colors.amber,
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        Text(
-          widget.prayer.name,
-          style: TextStyle(
-            fontSize: 30,
-            decoration:
-                prayerPassed ? TextDecoration.lineThrough : TextDecoration.none,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            prayerName,
+            style: TextStyle(
+              fontSize: 30,
+              color: prayerPassed ? Colors.grey : Colors.green,
+              fontWeight: FontWeight.w500,
+              decoration: TextDecoration.none,
+            ),
           ),
-        ),
-        Text(
-          widget.prayer.getTimeString(),
-          style: TextStyle(
-            fontSize: 30,
-            decoration:
-                prayerPassed ? TextDecoration.lineThrough : TextDecoration.none,
-          ),
-        )
-      ]),
+          Text(
+            prayerTime,
+            style: TextStyle(
+              fontSize: 30,
+              color: prayerPassed ? Colors.grey : Colors.green,
+              fontWeight: FontWeight.w400,
+              decoration: TextDecoration.none,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
