7e375b77d61030840a478eeaaeb73e7895ff82db
Burhan Ul Haq <burhanhaq3@gmail.com>
2023-08-12 19:53:03 -0700

    Move SettingsScreen to archive folder

--- lib/archive/home_screen.dart
+++ lib/archive/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:simple_azaan/widgets/clock_dial_widget.dart';
 import 'package:simple_azaan/models/prayer.dart';
 import 'package:simple_azaan/archive/prayer_card.dart';
 import 'package:simple_azaan/models/prayer_data.dart';
-import 'package:simple_azaan/screens/settings/settings_screen.dart';
+import 'package:simple_azaan/archive/settings_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
